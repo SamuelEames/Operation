f68e191bd56169cc009ead4905a25ef70c8f8249
Samuel Eames <samueljeames@gmail.com>
2021-10-31 17:07:43 +1000

    Tidied up code

--- Operation/Operation.ino
+++ Operation/Operation.ino
@@ -1,52 +1,60 @@
+/* OPERATION GAME
+    * Just your basic operation game
+        * Conductive edges of holes in person are connected to ground
+        * Metal tongs tethered to game with wire connected to TONGS_PIN
+        * Nose of operation guy lit up by pixel ring
+        * relay connects to buzzer
+    * Initially I was thinking of using capacitive touch as the sense method (using MPR121).
+      This has enough channels to connect each hole to a different input and then use pixel
+      LEDs inside to flash where the mistakes were made. Being capacitive touch people could
+      just use their hands to retrieve things and still trigger a fault.... BUT I didn't
+      have time to get that working initially, but this method works well
+*/
+
+// Libraries
 #include "FastLED.h"
 
-#define NUM_LEDS 12
-#define DATA_PIN 3
+// IO Setup
+#define TONGS_PIN 	4
+#define RELAY_PIN 	2
+#define LED_PIN 		3
 
+// Pixel setup
+#define NUM_LEDS 		12
 CRGB leds[NUM_LEDS];
 
+// Misc variables
+#define FLASH_TIME	250 		// (ms) Half cycle length of operation guy complaining
 
-int tongsPin = 4;  // Digital 2
-int relaypin = 2;
-int temp = 1;
 
-void setup(){
-  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
-  pinMode(tongsPin, INPUT);
-  pinMode(relaypin, OUTPUT);
-  digitalWrite(tongsPin, HIGH); //enable pullup resistor
-  digitalWrite(relaypin, LOW);
+void setup()
+{
+	FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, NUM_LEDS);
+	pinMode(TONGS_PIN, INPUT_PULLUP);
+	pinMode(RELAY_PIN, OUTPUT);
 
-  fill_solid( leds, NUM_LEDS, CRGB::Black);
-  FastLED.show();
+	digitalWrite(RELAY_PIN, LOW);
+
+	// Start with LEDs off
+	fill_solid(leds, NUM_LEDS, CRGB::Black);
+	FastLED.show();
 }
 
-void loop(){
-  
-  temp = digitalRead(tongsPin);
-  
-    if (temp == 0)
-    {
+void loop()
+{
+	if (!digitalRead(TONGS_PIN)) 												// If surgeon touched edge of hole in operation guy
+	{
+		for (uint8_t i = 0; i < 2; ++i) 									// Flash LEDs & buzzer twice
+		{
+			digitalWrite(RELAY_PIN, HIGH);  
+			fill_solid( leds, NUM_LEDS, CRGB::Red);
+			FastLED.show();
+			delay(FLASH_TIME);
 
-      digitalWrite(relaypin, HIGH);  
-      fill_solid( leds, NUM_LEDS, CRGB::Red);
-      FastLED.show();
-      delay(250);
-
-      digitalWrite(relaypin, LOW); 
-      fill_solid( leds, NUM_LEDS, CRGB::Black);
-      FastLED.show(); 
-      delay(250);
-
-      digitalWrite(relaypin, HIGH); 
-      fill_solid( leds, NUM_LEDS, CRGB::Red);
-      FastLED.show();
-      delay(250);
-
-      digitalWrite(relaypin, LOW);  
-      fill_solid( leds, NUM_LEDS, CRGB::Black);
-      FastLED.show(); 
-      delay(250);
-    }
-    delay(1);
+			digitalWrite(RELAY_PIN, LOW); 
+			fill_solid( leds, NUM_LEDS, CRGB::Black);
+			FastLED.show(); 
+			delay(FLASH_TIME);
+		}
+	}
 }
